b9782f0dd7dec84c5bfd56d2a45195fb7f52b279
Pidathala Pavani <pidathala.pavani@ibm.com>
2026-08-27 16:08:26 +0530

    Add ecommerce homepage

---
+++ ecommerce-homepage/src/Components/Banner.jsx
@@ -0,0 +1,13 @@
+function Banner() {
+  return (
+    <section>
+      <h2>Welcome to My Store</h2>
+      <img
+        src="https://placehold.co/1200x400"
+        alt="Store Banner"
+      />
+    </section>
+  );
+}
+
+export default Banner;
---
+++ ecommerce-homepage/src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+import ProductList from "../components/ProductList";
+import Footer from "../components/Footer";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <ProductList />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
